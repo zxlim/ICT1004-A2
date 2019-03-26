ccad99284fb2161d97a3a77e5fcb9664c2731c71
southzyzy <tanzhaoyea98.tzy@gmail.com>
2019-03-26 17:57:44 +0800

    search query

--- source_code/search.php
+++ source_code/search.php
@@ -17,14 +17,9 @@ require_once("serverside/components/search.php");
     <div class="container section_gap">
         <h2>Your search results for <?php safe_echo($search_query) ?></h2>
         <hr>
-        <?php
-        foreach ($results_listings as $row) {
-            foreach ($row["tags"] as $tag) {
-                ?>
-                <button type="button" class="btn btn-outline-secondary">$tag</button>
-                <?php
-            }}
-        ?>
+        <?php foreach ($results_listings as $row) { ?>
+            <button type="button" class="btn btn-outline-secondary"><?php safe_echo($row["tags"]); ?></button>
+        <?php } ?>
         <div class="col-xl-12 col-lg-8 col-md-7">
             <div class="row features-inner">
                 <?php if (sizeof($results_listings) === 0) { ?>
